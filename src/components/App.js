29a28fcb8cc985b73a3bc51d0852711f547b209a
ramanjit519 <43263426+ramanjit519@users.noreply.github.com>
2022-06-22 22:10:18 +0530

    .

--- src/components/App.js
+++ src/components/App.js
@@ -2,55 +2,15 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.cityList = [
-      { name: "Goa", country: "India" },
-      { name: "Amsterdam", country: "Netherlands" },
-      { name: "New York", country: "USA" },
-      { name: "Darjeeling", country: "India" },
-      { name: "Tokyo", country: "Japan" },
-      { name: "Lonavala", country: "India" },
-      { name: "Brandenburg Gate", country: "Germany" },
-      { name: "Reichstag Building", country: "Germany" },
-      { name: "Museum Island", country: "Germany" }
-      //  { name: "Munnar", country: "India" },
-      //  { name: "Leh Ladakh", country: "India" },
-      //  { name: "Goa", country: "India" },
-      // { name: "Agra", country: "India" },
-      // { name: "Dalhousie", country: "India" },
-      //  { name: "Coorg", country: "India" },
-      //  { name: "Rome", country: "Italy" },
-      //  { name: "Milan", country: "Italy" },
-      //  { name: "Venice", country: "Italy" },
-      //  { name: "Varanasai", country: "India" },
-      //  { name: "Jaipur", country: "India" },
-      //  { name: "The Hofburg", country: "Austria" },
-      //  { name: "Belvedere Palace", country: "Austria" },
-      //  { name: "St. Stephen Cathedral", country: "Austria" },
-      //  { name: "Kahna National Park", country: "India" },
-      //  { name: "Amritsar", country: "India" },
-      //  { name: "Mussoorie", country: "India" },
-      //  { name: "Mount Abu", country: "India" },
-      //  { name: "Tirupati", country: "India" }
-    ];
-
-    this.filterCityList = this.cityList.filter((item) => {
-      return item.country === "India";
-    });
-  }
-
   render() {
+    let relatives = ["Raman", "roshan", "parnkaj"];
     return (
       <div id="main">
-        <ol>
-          {this.filterCityList &&
-            this.filterCityList.map((item, index) => (
-              <li>
-                Key={`location${index + 1}`}&gt;{item.name}
-              </li>
-            ))}
+        {/* Do not remove the main div */}
+        <ol key="relativeList">
+          {relatives.map((relative, index) => (
+            <li key={`relativeListItem${index + 1}`}>{relative}</li>
+          ))}
         </ol>
       </div>
     );
